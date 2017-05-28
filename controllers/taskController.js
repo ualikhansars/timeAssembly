9997469507cd8f8dd959ff75e4a733af24e35dc8
ualikhansars <valikhan754@gmail.com>
2017-05-28 08:13:49 -0700

    Add Task Controller

---
+++ controllers/taskController.js
@@ -0,0 +1,54 @@
+var Task = require('../models/task');
+
+module.exports = {
+    find: function(params, callback) {
+        Task.find(params, function(err, tasks) {
+            if(err) {
+                callback(err, null);
+                return;
+            }
+            callback(null, tasks);
+        });
+    },
+
+    findById: function(id, callback) {
+        Task.findById(id, function(err, task) {
+            if(err) {
+                callback(err, null);
+                return;
+            }
+            callback(null, task);
+        })
+    },
+
+    create: function(params, callback) {
+        // logic to save into database
+        Task.create(params, function(err, task) {
+            if(err) {
+                callback(err, null);
+                return;
+            }
+            callback(null, task);
+        })
+    },
+
+    update: function(id, params, callback) {
+        Task.findByIdAndUpdate(id, params, {new: true}, function(err, task) {
+            if(err) {
+                callback(err, null);
+                return;
+            }
+            callback(null, task);
+        });
+    },
+
+    remove:  function(id, callback) {
+        Task.findByIdAndRemove(id, function(err, task) {
+            if(err) {
+                callback(err, null);
+                return;
+            }
+            callback(null, null);
+        })
+    }
+}
