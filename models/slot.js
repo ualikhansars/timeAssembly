6a4544216e4185b74052e91da4841c30535d6694
ualikhansars <valikhan754@gmail.com>
2017-05-31 04:51:44 -0700

    add total and free attr to slot model

--- models/slot.js
+++ models/slot.js
@@ -1,10 +1,13 @@
 var mongoose = require('mongoose');
+var User = require('./user');
 
 var slotSchema = mongoose.Schema({
     title: {type: String, required: true},
     category: {type: String},
+    total: {type: Number},
+    free: {type: Number},
     temporary: {type: Boolean, default: false},
-    dueData: {type: String},
+    dueDate: {type: String},
     username: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
