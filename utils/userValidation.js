7901bd2078ec351eae3eabcfc3ad52eb6543532e
ualikhansars <valikhan754@gmail.com>
2018-01-22 04:36:24 -0800

    move emailUserToken from config to utils

--- utils/userValidation.js
+++ utils/userValidation.js
@@ -5,31 +5,13 @@ var TimeFormat = require('../models/timeFormat');
 var TimeInterval = require('../models/timeInterval');
 import EmailVerificationToken from '../models/emailVerificationToken';
 
-import {generateEmailToken} from '../config/emailUserToken';
+import {generateEmailToken} from './emailUserToken';
 import {generateExpirationDate} from '../utils/generateExpirationDate';
 
 module.exports =  function validateUser(req, res) {
-    // User.findOne({email: req.body.email}, function(err, user) {
-    //   if(err) {
-    //     throw err;
-    //   }
-    //   console.log('user', user);
-    // });
-    
+
     req.checkBody('email', 'Enter correct email address').isEmail();
     req.checkBody('email', 'Email is required').notEmpty();
-    // req.checkBody('email').custom(val => {
-    //   User.findOne({ email: req.body.email}, (err, user) => {
-    //     if (err) console.log(err);
-    //     if(user == null) {
-    //       return;
-    //     } else {
-    //       throw new Error('this email is already in use');
-    //     }
-    //   });
-    // })
-    
-    
     req.checkBody('password', 'Password cannot be less than 4 characters').isLength({min: 4});
     req.checkBody('password', 'Password is required').notEmpty();
     req.checkBody('passwordConfirmation', 'Passwords do not match').equals(req.body.password);
